a0e7f6d3cf8218d3c0f3be54c8d5f259bd858707
rhassan19996 <70424287+rhassan19996@users.noreply.github.com>
2022-03-25 12:49:19 -0400

    Initial Commit

---
+++ IG_clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IG_clone
+//
+//  Created by Md Hashif on 3/25/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
